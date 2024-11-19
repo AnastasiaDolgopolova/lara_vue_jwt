60993ba68fd612ed2d2941fcd91f6e166523af42
AnastasiaDolgopolova <anastasiadolgopolova11@gmail.com>
2024-11-19 12:44:41 +0400

    add fruit list

--- resources/js/components/Fruit/IndexComponent.vue
+++ resources/js/components/Fruit/IndexComponent.vue
@@ -1,13 +1,47 @@
 <template>
     <div>
-        list of fruits
+        <table class="table">
+            <thead>
+            <tr>
+                <th scope="col">#</th>
+                <th scope="col">Name</th>
+                <th scope="col">Price</th>
+            </tr>
+            </thead>
+            <tbody>
+            <template v-for="fruit in fruits">
+                <tr>
+                    <th scope="row">{{fruit.id}}</th>
+                    <td>{{fruit.name}}</td>
+                    <td>{{fruit.price}}</td>
+                </tr>
+            </template>
+            </tbody>
+        </table>
     </div>
 </template>
 
 <script>
 
 export default {
-    name: "IndexComponent"
+    name: "IndexComponent",
+
+    data() {
+        return {
+            fruits: null,
+        }
+    },
+    mounted() {
+        this.getFruits();
+    },
+    methods: {
+        getFruits() {
+            axios.get('/api/fruits')
+                .then(res => {
+                    this.fruits = res.data.data;
+                })
+        },
+    }
 }
 
 </script>
